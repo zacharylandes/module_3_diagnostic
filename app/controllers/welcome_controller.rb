406bd2bf4807561cf0d6b20e43474987f42f73bc
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-07 09:24:17 -0700

    set up test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @search = NrelService.new(params['search'])
   end
 end
---
+++ spec/features/user_can_locate_nearby_stations_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature "User visits root page" do
+  scenario "user inputs zipcode " do
+    it 'can locate the 10 closest stations '
+    visit "/"
+
+    fill_in :
+
+
+    within(".navbar") do
+      expect(page).to have_content("AltFuelFinder")
+      expect(page).to have_selector("input[value='Search by zip...']")
+    end
+  end
+
+end
+
+
+# ```As a user
+# When I visit "/"
+# And I fill in the search form with 80203
+# And I click "Locate"
+# Then I should be on page "/search" with parameters visible in the url
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times```
